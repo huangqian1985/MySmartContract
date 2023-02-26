6cb9b5168cab35ee0d93c9cdd1d7627ea533e62d
huangqian <huangqian1985@qq.com>
2023-02-26 22:26:48 +0800

    NaughtyHamsterServer 提交 提供了 getTotalSupply接口获取合约当前已mint数量

--- Golang/NaughtyHamsterServer/go.mod
+++ Golang/NaughtyHamsterServer/go.mod
@@ -2,22 +2,42 @@ module NaughtyHamsterServer
 
 go 1.19
 
-require github.com/ethereum/go-ethereum v1.11.1
+require (
+	github.com/ethereum/go-ethereum v1.11.1
+	github.com/gogf/gf/v2 v2.3.2
+)
 
 require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/clbanning/mxj/v2 v2.5.5 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
+	go.opentelemetry.io/otel v1.7.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.7.0 // indirect
+	go.opentelemetry.io/otel/trace v1.7.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- Golang/NaughtyHamsterServer/main.go
+++ Golang/NaughtyHamsterServer/main.go
@@ -1,19 +1,125 @@
 package main
 
 import (
+	naughtyHamster "NaughtyHamsterServer/NaughtyHamster"
 	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
-	"log"
-
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/gogf/gf/v2/os/glog"
+	"github.com/gogf/gf/v2/os/gtimer"
+	"log"
+	"time"
+)
+
+const (
+	TotalSupply = "/getTotalSupply"
+)
+
+var (
+	client               *ethclient.Client
+	logger               *glog.Logger
+	ctx                  = gctx.New()
+	rpcURL, contractAddr string
+	serverPort           int
+	totalSupply          int64
 )
 
 func main() {
-	client, err := ethclient.Dial("https://eth-goerli.g.alchemy.com/v2/Yy5Huxu5D7mM0_BigYQzKoiPVCxBw69p")
+	initLog()
+
+	if err := initConfig(); err != nil {
+		panic(err)
+	}
+	if err := initClient(); err != nil {
+		panic(err)
+	}
+
+	startTimer()
+
+	s := g.Server()
+	group := s.Group("")
+	group.GET(TotalSupply, func(req *ghttp.Request) {
+		req.Response.Write(getTotalSupply())
+	})
+	s.SetPort(serverPort)
+	s.Run()
+}
+
+func initConfig() error {
+	if val, err := g.Config().Get(ctx, "ethereum.serverPort"); err != nil {
+		return err
+	} else {
+		serverPort = val.Int()
+	}
+
+	if val, err := g.Config().Get(ctx, "ethereum.rpcURL"); err != nil {
+		return err
+	} else {
+		rpcURL = val.String()
+	}
+
+	if val, err := g.Config().Get(ctx, "ethereum.contractAddr"); err != nil {
+		return err
+	} else {
+		contractAddr = val.String()
+	}
+	logger.Debug(ctx, "initConfig finish")
+	return nil
+}
+
+func initClient() error {
+	var err error
+	client, err = ethclient.Dial(rpcURL)
+	if err != nil {
+		return err
+	}
+	logger.Debug(ctx, "initClient finish")
+	return nil
+}
+
+func initLog() {
+	logger = glog.New()
+	logger.SetConfigWithMap(g.Map{
+		"path":     "log",
+		"level":    "all",
+		"StStatus": 0,
+	})
+	logger.Debug(ctx, "initlog finish")
+}
+
+func startTimer() {
+	getTotalSupplyFromEthereum()
+	gtimer.Add(ctx, time.Minute, func(ctx context.Context) {
+		getTotalSupplyFromEthereum()
+		logger.Debug(ctx, fmt.Sprintf("startTimer getTotalSupplyFromEthereum = %d", totalSupply))
+	})
+	logger.Debug(ctx, "startTimer finish")
+}
+
+func getTotalSupply() int64 {
+	return totalSupply
+}
+
+func getTotalSupplyFromEthereum() {
+	address := common.HexToAddress(contractAddr)
+	instance, err := naughtyHamster.NewNaughtyHamster(address, client)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	val, err := instance.TotalSupply(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	totalSupply = val.Int64()
+}
+
+func test(client *ethclient.Client) {
 	// Get the balance of an account
 	account := common.HexToAddress("0xd6bAe01Bce8ecc43E5d291172b2029e8c9acE58d")
 	balance, err := client.BalanceAt(context.Background(), account, nil)
